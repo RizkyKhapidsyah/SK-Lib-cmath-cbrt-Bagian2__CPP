c6e6a6e8ceeb7a6d73310f245c72ecf89ca66044
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-10 08:04:28 +0700

    Add project files.

---
+++ SK-Lib-cmath-cbrt-Bagian2__CPP/Source.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	int x1 = 7;
+	cout << cbrt(x1) << endl;
+
+	double x2 = 3.78;
+	cout << cbrt(x2) << endl;
+
+	long long x3 = 2;
+	cout << cbrt(x3);
+
+	_getch();
+	return 0;
+}
